40c2ae38b7278165dbd96def0ad1908114629781
RobinBhalla <robinbhalla95@yahoo.com>
2023-05-28 22:00:15 -0500

    Function check

---
+++ hello.js
@@ -0,0 +1,11 @@
+const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
